46f6b15623f0023b543c992ba8c3a64ae9e614ea
Mariya Dolganova <dolganovaya@yandex.ru>
2024-08-04 20:50:17 +0300

    add brain-prime

--- src/Games/PrimeGame.php
+++ src/Games/PrimeGame.php
@@ -2,6 +2,31 @@
 
 namespace Php\Project\Games\PrimeGame;
 
+use function Php\Project\Engine\engine;
+
 function primeGame()
 {
+    $question = function () {
+        $question = rand(0, 101);
+        return $question;
+    };
+
+    $right = function ($question) {
+        $number = (int) $question;
+        if ($number === 0) {
+          return 'no';
+        }
+        if ($number === 1) {
+          return 'yes';
+        }
+        for ($i = 2; $i < $number; $i++) {
+          if ($number % $i === 0) {
+            return 'no';
+          }
+        }
+        return 'yes';
+    };
+
+    $rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    engine($rules, $question, $right);
 }
