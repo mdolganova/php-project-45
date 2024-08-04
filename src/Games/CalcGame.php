b7a88b14904c3395044a1b0d585092063d17a8db
Mariya Dolganova <dolganovaya@yandex.ru>
2024-08-04 20:15:00 +0300

    add brain-calc

--- src/Games/CalcGame.php
+++ src/Games/CalcGame.php
@@ -2,6 +2,40 @@
 
 namespace Php\Project\Games\CalcGame;
 
+use function Php\Project\Engine\engine;
+
 function calcGame()
 {
+    $question = function () {
+        $operations = ['+', '-', '*'];
+        $operationNumber = rand(0, 2);
+        $operation = $operations[$operationNumber];
+        $x = rand(0, 101);
+        $y = random_int(0, 101);
+        $expression = "{$x} {$operation} {$y}";
+        return $expression;
+    };
+
+    $right = function ($question) {
+        $elements = explode(' ', $question);
+        $x = (int) $elements[0];
+        $y = (int) $elements[2];
+        $operand = $elements[1];
+        switch ($operand) {
+          case '+':
+            $result = $x + $y;
+            break;
+          case '-':
+            $result = $x - $y;
+            break;
+          case '*':
+            $result = $x * $y;
+            break;
+          default:
+        }
+        return (string) $result;
+    };
+
+    $rules = 'What is the result of the expression?';
+    engine($rules, $question, $right);
 }
